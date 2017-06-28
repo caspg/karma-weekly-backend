8aa272ab2f8a62954e101e8c5d1cfe0d7efca723
Kacper Golinski <kacper.golinski@gmail.com>
2017-06-28 23:09:45 +0200

    create emailLogin.test.js

---
+++ src/services/graphql/schema/auth/mutations/emailLogin.test.js
@@ -0,0 +1,37 @@
+const {
+  GraphQLString,
+  GraphQLInt,
+} = require('graphql');
+
+const emailLoginFactory = require('./emailLogin');
+
+describe('emailLogin mutation', () => {
+  const emailLoginServiceMock = jest.fn();
+  const authService = { emailLogin: emailLoginServiceMock };
+
+  const emailLogin = emailLoginFactory(authService);
+
+  it('has correct type', () => {
+    const emailLoginTypeFields = emailLogin.type.getFields();
+    const fieldsNames = Object.keys(emailLoginTypeFields);
+
+    expect(fieldsNames).toEqual(['error', 'status']);
+    expect(emailLoginTypeFields.error.type).toEqual(GraphQLString);
+    expect(emailLoginTypeFields.status.type).toEqual(GraphQLInt);
+  });
+
+  it('accepets correct arguments', () => {
+    const emailLoginArgsFields = emailLogin.args;
+    const fieldsNames = Object.keys(emailLoginArgsFields);
+
+    expect(fieldsNames).toEqual(['email']);
+    expect(emailLoginArgsFields.email.type).toEqual(GraphQLString);
+  });
+
+  it('calls authService.emailLogin in resolver', () => {
+    const email = 'some@email.pl';
+    emailLogin.resolve(null, { email });
+
+    expect(emailLoginServiceMock).toHaveBeenCalledWith(email);
+  });
+});
