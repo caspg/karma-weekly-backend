6f60abcce134c28e7d70354d740c78bd37320118
Kacper Golinski <kacper.golinski@gmail.com>
2017-06-28 23:00:04 +0200

    create graphql/schema/auth test

--- src/graphqlHandler.js
+++ src/graphqlHandler.js
@@ -4,7 +4,7 @@ const graphqlServiceFactory = require('./services/graphql');
 const graphqlService = graphqlServiceFactory(authService);
 
 function graphqlHandler(event, context, callback) {
-  console.log('Received event', event);
+  console.log('Received event:', event);
 
   return graphqlService.runQuery(event.body.query)
     .then(response => callback(null, response))
