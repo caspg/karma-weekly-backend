b189b81e375b67ced2199830c5b3e8b0826541a3
Kacper Golinski <kacper.golinski@gmail.com>
2017-07-24 20:32:27 +0200

    create getJsonContent service

---
+++ src/services/getJsonContent.js
@@ -0,0 +1,29 @@
+const https = require('https');
+
+/**
+ * @param {string} url
+ * @returns {promise}
+ */
+function getJsonContent(url) {
+  if (!url.startsWith('https')) {
+    throw Error('Provided url must starts with "https".');
+  }
+
+  return new Promise((resolve, reject) => {
+    const request = https.get(url, (response) => {
+      if (response.statusCode < 200 || response.statusCode > 299) {
+        reject(new Error(`Faild to load page, status code: ${response.statusCode}`));
+      }
+
+      const body = [];
+
+      response.on('data', chunk => body.push(chunk));
+
+      response.on('end', () => resolve(body.join('')));
+    });
+
+    request.on('error', error => reject(error));
+  });
+}
+
+module.exports = getJsonContent;
