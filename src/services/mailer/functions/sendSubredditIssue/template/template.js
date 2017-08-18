bcf57faa0abade574268cec511ca2b45354752c4
Kacper Golinski <kacper.golinski@gmail.com>
2017-08-18 19:59:15 +0200

    update unsubscribeUrl

--- src/services/mailer/functions/sendSubredditIssue/template/template.js
+++ src/services/mailer/functions/sendSubredditIssue/template/template.js
@@ -3,9 +3,19 @@ const header = require('./components/header');
 const footer = require('./components/footer');
 const subredditLinkRow = require('./components/subredditLinkRow');
 
+function makeUnsubscribeUrl() {
+  const baseUrl = process.env.BASE_URL;
+
+  if (!baseUrl) {
+    throw Error('BASE_URL env variable must be specified!');
+  }
+
+  return `${baseUrl}/dashboard`;
+}
+
 function template(subredditIssue) {
   const { name, date, links } = subredditIssue;
-  const unsubscribeUrl = 'https//google.com'; // TODO
+  const unsubscribeUrl = makeUnsubscribeUrl();
 
   const linksContent = links
     .map(link => subredditLinkRow(link))
