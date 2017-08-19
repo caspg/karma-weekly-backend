a693321763ee6a44b01c7c615f5142521fd59186
Kacper Golinski <kacper.golinski@gmail.com>
2017-08-19 14:17:59 +0200

    update aws roles

--- src/services/auth/functions/emailLogin.js
+++ src/services/auth/functions/emailLogin.js
@@ -25,7 +25,7 @@ function emailLoginFactory(usersService, mailerService) {
     }
 
     const shortLiveToken = createShortLiveJwt(email);
-    const loginUrl = `${baseUrl}/magic/login?m=${shortLiveToken}`;
+    const loginUrl = `${baseUrl}/magic/login/?m=${shortLiveToken}`;
 
     return usersService
       .findOrCreateUser(email)
